0e3e2f6ff6628a2422cdb91b44cf6fd328cf4979
jio-H <1463388724@qq.com>
2025-11-04 22:08:58 +0800

    deadlock modified

--- deadlock/deadlock.cpp
+++ deadlock/deadlock.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#define _GNU_SOURCE
+#include <dlfcn.h>
 
 //锁与线程的信息
 struct pair_t {
@@ -62,99 +64,6 @@ struct task_graph {
 };
 
 struct task_graph *tg = NULL;
-
-
-
-
-
-void lock_before(unsigned long int thread_id, unsigned long int lock) {
-    pthread_mutex_lock_f(&tg->mutex);
-
-    int idx = search_lock(lock);
-//    printf("[lock_before] self_id:%lu lock:%lu lock idx:%d \n", thread_id, lock, idx);
-    //如果该锁是第一次则什么都不做
-    if (idx != -1) {
-        //u是想要加锁的线程
-        struct pair_t u;
-        u.th_id = thread_id;
-        u.type = PROCESS;
-        //把vertex添加到vertex_list里面
-        add_verrtex(u);
-        //v是锁原来的线程
-        struct pair_t v;
-        v.th_id = tg->lock_list[idx].th_id;
-        tg->lock_list[idx].degress++;
-        v.type = PROCESS;
-        add_vertex(v);
-
-        if (!verify_edge(u, v)) {
-            add_edge(u, v); // 把v加入到vertex_list的u的链表中
-        }
-    }
-
-    pthread_mutex_unlock_f(&tg->mutex);
-}
-
-void lock_after(unsigned long int thread_id, unsigned long int lock) {
-    pthread_mutex_lock_f(&tg->mutex);
-
-    int idx = search_lock(lock);
-//    printf("[lock_after ] self_id:%lu lock:%lu ", thread_id, lock);
-
-    if (idx == -1) {  // 第一次加锁,找一个空位lock_list，设置th_id和lock
-        int empty_lock_idx = search_empty_lock(lock);
-        tg->lock_list[empty_lock_idx].th_id = thread_id;
-        tg->lock_list[empty_lock_idx].lock_id = lock;
-//        printf("分配lock_list位置 idx:%d \n", empty_lock_idx);
-        if (empty_lock_idx >= tg->lock_num) {
-            inc(&tg->lock_num, 1);
-        }
-    }
-    else {
-        //u是想要加锁的线程
-        struct pair_t u;
-        u.th_id = thread_id;
-        u.type = PROCESS;
-        //v是锁原来的线程
-        struct pair_t v;
-        v.th_id = tg->lock_list[idx].th_id;
-        tg->lock_list[idx].degress--;
-        v.type = PROCESS;
-        //删除边
-        if (verify_edge(u, v)) {
-            remove_edge(u, v);
-        }
-        //设为本线程
-        tg->lock_list[idx].th_id = thread_id;
-
-//        printf("获得 lock idx:%d \n", idx);
-    }
-
-    pthread_mutex_unlock_f(&tg->mutex);
-}
-
-void unlock_after(unsigned long int thread_id, unsigned long int lock) {
-    pthread_mutex_lock_f(&tg->mutex);
-
-    int idx = search_lock(lock);
-    //如果入度为0，说明没有别的线程指向该锁，则把这个idx位置置空
-    if (tg->lock_list[idx].degress == 0) {
-        tg->lock_list[idx].th_id = 0;
-        tg->lock_list[idx].lock_id = 0;
-    }
-
-    pthread_mutex_unlock_f(&tg->mutex);
-}
-
-
-int pthread_mutex_lock(pthread_mutex_t *mutex) {
-    pthread_t self_id = pthread_self(); //
-    lock_before(self_id, (unsigned long int) mutex);
-    pthread_mutex_lock_f(mutex);
-    lock_after(self_id, (unsigned long int) mutex);
-}
-
-
 
 //创建一个vertex
 struct vertex_t *create_vertex(struct pair_t pair) {
@@ -243,6 +152,127 @@ int remove_edge(struct pair_t u, struct pair_t v) {
         }
     }
 }
+
+
+
+//查找lock在list里面的下标
+int search_lock(unsigned long int lock) {
+    int i = 0;
+
+    for (i = 0; i < tg->lock_num; i++) {
+        if (tg->lock_list[i].lock_id == lock) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+//查找lock在list里面的下标
+int search_empty_lock(unsigned long int lock) {
+    int i = 0;
+
+    for (i = 0; i < tg->lock_num; i++) {
+        if (tg->lock_list[i].degress == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+
+void lock_before(unsigned long int thread_id, unsigned long int lock) {
+    pthread_mutex_lock_f(&tg->mutex);
+
+    int idx = search_lock(lock);
+//    printf("[lock_before] self_id:%lu lock:%lu lock idx:%d \n", thread_id, lock, idx);
+    //如果该锁是第一次则什么都不做
+    if (idx != -1) {
+        //u是想要加锁的线程
+        struct pair_t u;
+        u.th_id = thread_id;
+        u.type = PROCESS;
+        //把vertex添加到vertex_list里面
+        add_vertex(u);
+        //v是锁原来的线程
+        struct pair_t v;
+        v.th_id = tg->lock_list[idx].th_id;
+        tg->lock_list[idx].degress++;
+        v.type = PROCESS;
+        add_vertex(v);
+
+        if (!verify_edge(u, v)) {
+            add_edge(u, v); // 把v加入到vertex_list的u的链表中
+        }
+    }
+
+    pthread_mutex_unlock_f(&tg->mutex);
+}
+
+void lock_after(unsigned long int thread_id, unsigned long int lock) {
+    pthread_mutex_lock_f(&tg->mutex);
+
+    int idx = search_lock(lock);
+//    printf("[lock_after ] self_id:%lu lock:%lu ", thread_id, lock);
+
+    if (idx == -1) {  // 第一次加锁,找一个空位lock_list，设置th_id和lock
+        int empty_lock_idx = search_empty_lock(lock);
+        tg->lock_list[empty_lock_idx].th_id = thread_id;
+        tg->lock_list[empty_lock_idx].lock_id = lock;
+//        printf("分配lock_list位置 idx:%d \n", empty_lock_idx);
+        // if (empty_lock_idx >= tg->lock_num) {
+        //     inc(&tg->lock_num, 1);
+        // }
+    }
+    else {
+        //u是想要加锁的线程
+        struct pair_t u;
+        u.th_id = thread_id;
+        u.type = PROCESS;
+        //v是锁原来的线程
+        struct pair_t v;
+        v.th_id = tg->lock_list[idx].th_id;
+        tg->lock_list[idx].degress--;
+        v.type = PROCESS;
+        //删除边
+        if (verify_edge(u, v)) {
+            remove_edge(u, v);
+        }
+        //设为本线程
+        tg->lock_list[idx].th_id = thread_id;
+
+//        printf("获得 lock idx:%d \n", idx);
+    }
+
+    pthread_mutex_unlock_f(&tg->mutex);
+}
+
+void unlock_after(unsigned long int thread_id, unsigned long int lock) {
+    pthread_mutex_lock_f(&tg->mutex);
+
+    int idx = search_lock(lock);
+    //如果入度为0，说明没有别的线程指向该锁，则把这个idx位置置空
+    if (tg->lock_list[idx].degress == 0) {
+        tg->lock_list[idx].th_id = 0;
+        tg->lock_list[idx].lock_id = 0;
+    }
+
+    pthread_mutex_unlock_f(&tg->mutex);
+}
+
+
+int pthread_mutex_lock(pthread_mutex_t *mutex) {
+    pthread_t self_id = pthread_self(); //
+    lock_before(self_id, (unsigned long int) mutex);
+    pthread_mutex_lock_f(mutex);
+    lock_after(self_id, (unsigned long int) mutex);
+}
+
+
+
+
 
 /* ******* ******************check cycle****************** ******* */
 
